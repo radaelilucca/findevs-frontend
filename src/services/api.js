d239ebc5c538e860ce8876f061676032f0078f04
Lucca Radaeli <radaelidev@MacBook-Pro-de-Lucca.local>
2020-01-22 22:29:27 -0800

    Changed api adress

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'https://findev-omniweek.herokuapp.com/',
+  baseURL: 'https://findevs-backend.herokuapp.com/',
 });
 
 export default api;
