3d0d4070329a33606a88f72d88602c28216455d5
radaelilucca <radaelidev@gmail.com>
2020-01-26 21:53:16 -0800

    Folders structure to pages, setup routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Routes from './routes'
+
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
+import {getToken} from './auth'
+
 const api = axios.create({
-  baseURL: 'https://findevs-backend.herokuapp.com/',
+  baseURL: 'http://localhost:3333',
 });
+
+api.interceptors.request.use(async config => {
+
+  const token = getToken()
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config;
+})
+
 
 export default api;
