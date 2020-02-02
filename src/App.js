a2696bdb2a0492ca9109f419019dc638fabe2690
radaelilucca <radaelidev@gmail.com>
2020-02-01 16:46:11 -0800

    Revert "Inserted infinite scroll"
    This reverts commit fe4df85591486a3f585b387b6699b15ca879b097.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,54 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-filename-extension */
-import React, { useState, useEffect } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
-import Loader from "react-loader-spinner";
+import React, { useState, useEffect } from 'react';
 
-import api from "./services/api";
-import "./global.css";
-import "./App.css";
-import "./Sidebar.css";
-import "./Main.css";
+import api from './services/api';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
-import DevItem from "./components/DevItem";
-import DevForm from "./components/DevForm";
-import logo from "./assets/Logo3.png";
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+import logo from './assets/Logo3.png';
+
 
 function App() {
   const [devs, setDevs] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loadMore, setLoadMore] = useState(false);
 
   useEffect(() => {
     async function loadDevs() {
-      const response = await api.get("/devs", {
-        params: {
-          page,
-          limit: 6
-        }
-      });
+      const response = await api.get('/devs');
 
-      if (response.data.length) {
-        setDevs(devs.concat(response.data));
-        setLoadMore(true);
-      } else {
-        setLoadMore(false);
-      }
+      setDevs(response.data);
     }
 
     loadDevs();
-  }, [page]);
+  }, [devs]);
 
   async function handleAddDev(data) {
-    const response = await api.post("/devs", data);
+    const response = await api.post('/devs', data);
 
     setDevs([...devs, response.data]);
   }
 
   async function handleInativeDev(data) {
-    console.log(data);
 
+    console.log(data)
+    
     await api.put(`/devs/delete/${data}`);
-    console.log(data.github_user);
+    console.log(data.github_user)
+    
 
-    const filterDevs = devs.filter(dev => dev.github_user !== data.github_user);
+    const filterDevs = devs.filter(
+      dev => dev.github_user !== data.github_user
+    );
 
-    console.log(filterDevs);
+    console.log(filterDevs)
 
     setDevs(filterDevs);
   }
+
 
   return (
     <div id="app">
@@ -65,30 +57,11 @@ function App() {
         <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
-        <InfiniteScroll
-          dataLength={devs.length}
-          next={() => {
-            setPage(page + 1);
-          }}
-          hasMore={loadMore}
-          loader={
-            <div className="loader">
-              <Loader
-                type="Oval"
-                color="#8979EE"
-                height={50}
-                width={50}
-                timeout={50000} //3 secs
-              />
-            </div>
-          }
-        >
-          <ul>
-            {devs.map(dev => (
-              <DevItem dev={dev} key={dev._id} deletar={handleInativeDev} />
-            ))}
-          </ul>
-        </InfiniteScroll>
+        <ul>
+          {devs.map(dev => (
+            <DevItem dev={dev} key={dev._id} deletar={handleInativeDev}/>
+          ))}
+        </ul>
       </main>
     </div>
   );
